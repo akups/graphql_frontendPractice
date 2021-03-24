f5f26edc5a1fbd230310f3aed247afe092be1ddd
akups <appiah-lindberg@outlook.com>
2021-03-24 08:46:51 +0100

    changin our endpoint from localhost to deployed version

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://thawing-sands-35319.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
